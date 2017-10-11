7b8addc1c4e7a72b328781cafe1eda8a574b12ff
Yonatan Cohen <yonatanc@mellanox.com>
2017-10-11 11:00:42 +0300

    startup script
    mkyonienv.sh is invoked right after cloning yonienv
    it sets up .bash_profile .bashrc and vim env.

---
+++ mkyonienv.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+
+setup_bash_profile ()
+{
+    local yonienv=$1;
+    echo "export PATH=$PATH:${yonienv}/bin" >> ~/.bash_profile;
+}
+
+setup_bashrc () 
+{
+    local yonienv=$1;
+    echo "export yonienv=${yonienv};" >> ~/.bashrc;
+    echo "source ${yonienv}/bashrc_main.sh;" >> ~/.bashrc;
+}
+
+setup_vim_env () 
+{
+    local yonienv=$1;
+    ln -snf $(yonienv)/vim ~/.vim;
+    echo "source ~/.vim/.vimrc" >> ~/.vimrc;
+}
+
+main  ()
+{
+    local yonienv=$1;
+
+    if [ -z ${yonienv} ] ; then 
+        echo "you must give the path to yonienv";
+        echo "bailing out...";
+        return;
+    fi
+
+    setup_bash_profile ${yonienv};
+    setup_bashrc ${yonienv};
+}
+
+main $@;
