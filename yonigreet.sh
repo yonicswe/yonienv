694c8d239cd84bf4e69fd16d3a8d2ec2689db8b2
Yonatan Cohen <yonatanc@mellanox.com>
2017-10-19 17:00:56 +0300

    linux greeting

--- yonigreet.sh
+++ yonigreet.sh
@@ -1,1 +1,24 @@
-cowsay -f tux $(fortune)
+
+
+# sudo yum install cowsay fortune-mod -y
+# sudo cp me to /etc/profile.d/
+
+cow=0;
+msg=0;
+yonimsg="welcome to yoni cohen setup...";
+rpm --verify cowsay 1>/dev/null
+ret=$?
+[ $ret -eq 0 ] && cow=1
+rpm --verify fortune-mod 1>/dev/null
+ret=$?
+[ $ret -eq 0 ] && msg=1
+
+if (( $cow == 1 && $msg == 1 )) ; then 
+    cowsay -f tux $(fortune)
+elif (( $cow == 1 )) ; then 
+    cowsay -f tux "echo $yonimsg";
+elif (( $msg == 1 )) ; then 
+    /usr/bin/fortune
+else
+    echo $yonimsg;
+fi    
