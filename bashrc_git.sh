2c62715b865ec1b16f342c54110faac490783447
Yonatan Cohen <yonatanc@mellanox.com>
2017-10-26 08:41:03 +0300

    git aliases

--- bashrc_git.sh
+++ bashrc_git.sh
@@ -7,6 +7,9 @@ alias gitdiffstat='git --no-pager diff --stat'
 alias gitlog='git log --name-status'
 alias gitmkhook='gitdir=$(git rev-parse --git-dir); scp -p -P 29418 yonatanc@l-gerrit.mtl.labs.mlnx:hooks/commit-msg ${gitdir}/hooks/'
 alias gitd='git d'
+alias gitdiscardunstaged='git checkout -- .'
+alias gitstashunstaged='git stash --keep-index'
+alias gitunstageall='git reset HEAD'
 gitb () 
 {
     git b | awk '/^\*/{print $1" "$2} !/^\*/{print "  "$1}'
