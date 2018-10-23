yonicswe/yonienv

commit e1ffd05e15a2957d36f6d2003a6a0e87b5a9a15a

main: ignore changes in private tracket files
env_common_args
vim/vimrc.guifont

although these files are tracked, the user
can change them to match his setup
and there is no need to see these changes
with git status

Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

bashrc_main.sh
@@ -20,3 +20,6 @@ source ${yonienv}/bashrc_git.sh
 # office related 
 # source ${yonienv}/bashrc_nice.sh
 source ${yonienv}/bashrc_mlx.sh
+
+git update-index --assume-unchanged env_common_args.sh
+git update-index --assume-unchanged vim/vimrc.guifont

commit 39a3c6264a80a1062c21bda3ad8f93226faa3db1

git: ignore changes in specific git files.
this should be done when the environment is
created.

Fixes: e1ffd05
Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

bashrc_main.sh
@@ -20,6 +20,3 @@ source ${yonienv}/bashrc_git.sh
 # office related 
 # source ${yonienv}/bashrc_nice.sh
 source ${yonienv}/bashrc_mlx.sh
-
-git update-index --assume-unchanged env_common_args.sh
-git update-index --assume-unchanged vim/vimrc.guifont

mkyonienv.sh
@@ -53,6 +53,9 @@ setup_git_env ()
     local yonienv=$1;
     echo $FUNCNAME
     ln -snf ${yonienv}/gitconfig ~/.gitconfig;
+
+    git update-index --assume-unchanged env_common_args.sh
+    git update-index --assume-unchanged vim/vimrc.guifont
 }
 
 setup_cgdb_env () 