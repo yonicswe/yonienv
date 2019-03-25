00b1075b02da239e36f067876e4e0742c52cb80b
Yonatan Cohen <yonatanc@mellanox.com>
2019-03-25 08:33:44 +0200

    misc: Added aliases for weather
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bashrc_misc.sh
+++ bashrc_misc.sh
@@ -17,3 +17,33 @@ fi
 
 # pdfgrep to grep in pdf files.
 # pdfgrep -n <pattern> <filename>
+
+# to get help using wttr.in
+#  curl -s "wttr.in/:help"
+_weather(){ curl -s "wttr.in?m1" ;}
+
+wttr()
+{
+# change Paris to your default location
+    local request="wttr.in/${1-Paris}";
+    [ "$COLUMNS" -lt 125 ] && request+='?n';
+    curl -H "Accept-Language: ${LANG%_*}" --compressed "$request";
+}
+
+function count() {
+      total=$1
+            for ((i=total; i>0; i--)); do sleep 1; printf "Time remaining $i secs \r"; done
+                  echo -e "\a"
+}
+
+function up() {
+    times=$1;
+    while [ "$times" -gt "0" ]; do
+        cd ..
+        times=$(($times - 1));
+    done
+}
+
+# cat "file" with color 
+# alias ccat="source-highlight --out-format=esc256 -o STDOUT -i"
+
