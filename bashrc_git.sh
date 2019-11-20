caa22dee2d1b15d582753014d6462a564c751076
Yonatan Cohen <yonatanc@mellanox.com>
2019-11-20 11:11:23 +0000

    git: gitcommityoni has new argument
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bashrc_git.sh
+++ bashrc_git.sh
@@ -158,9 +158,15 @@ gitcatpatchfile ()
 
 gitcommityoni ()
 {
+    local module=${1};
+    if [ -n "${module}" ] ; then 
+        sed -i "s/^module/${module}/g" ${yonienv}/git_templates/git_commit_template_yonic;
+    fi
+
     git config commit.template ${yonienv}/git_templates/git_commit_template_yonic; 
     git commit;
     git config --unset commit.template;
+    git checkout ${yonienv}/git_templates/git_commit_template_yonic;
 }
 
 gitcommitmlx ()
