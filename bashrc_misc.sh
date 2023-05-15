d555f8e676cc9e333fee7016ffc0e4d803b796ed
yonatan.cohen <yonatan.cohen@dell.com>
2023-05-15 08:49:57 +0300

    misc: alias for installing zsh zap environment
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bashrc_misc.sh
+++ bashrc_misc.sh
@@ -330,3 +330,8 @@ findvideofiles ()
             basename $x ; 
         done;
 }
+
+install-zsh-zap ()
+{
+    zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+}
