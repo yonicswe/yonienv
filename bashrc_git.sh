3ff94172e887385cb752a5c7ad8eaf72a7139718
yonatan.cohen <yonatan.cohen@dell.com>
2023-09-19 14:24:28 +0300

    git: checkoutremotebranch asks user to rename local branch
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bashrc_git.sh
+++ bashrc_git.sh
@@ -263,29 +263,43 @@ git-checkoutbranch ()
 
 git-checkoutremotebranch ()
 {
-    local branch=;
+    local remote_branch=;
+    local local_branch=;
 
     ask_user_default_no "git fetch before we start ? ";
     if [ $? -eq 1 ] ; then
         git fetch -p;
     fi;
 
-    branch="$(git b -r |sed 's/.*origin\///g'| fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+    remote_branch="$(git b -r |sed 's/.*origin\///g'| fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
 
-    if [[ -z ${branch} ]] ; then
+    if [[ -z ${remote_branch} ]] ; then
         echo "you must specify a valid branch";
         return -1;
     fi;
 
-    if [ $( git b | grep ${branch} | wc -l ) -gt 0 ] ; then
-        echo "${branch} already checked out !! remove it and try again";
+    echo "remote branch : ${remote_branch}";
+
+    ask_user_default_no "rename the branch ? ";
+    if [ $? -eq 1 ] ; then
+        read -p "new name : " local_branch;
+    else
+        local_branch=${remote_branch};
+    fi;
+
+    if [ $( git b | grep ${local_branch} | wc -l ) -gt 0 ] ; then
+        echo "${local_branch} already checked out !! remove it and try again";
         return;
     fi;
 
-    echo -n "git checkout ${branch}";
-    ask_user_default_no;
+    echo "git fetch origin ${remote_branch}";
+    echo "git checkout -b ${local_branch} FETCH_HEAD";
+    ask_user_default_no "continue";
     if [ $? -eq 0 ] ; then return -1; fi;
-    git checkout ${branch};
+
+    git fetch origin ${remote_branch};
+    git checkout -b ${local_branch} FETCH_HEAD;
+
     return 0;
 }
 
