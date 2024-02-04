81c2e337caba8c6659309faca796d97c3de1f014
yonatan.cohen <yonatan.cohen@dell.com>
2024-02-04 07:40:17 +0200

    git: git-deletemultiplebranches
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bashrc_git.sh
+++ bashrc_git.sh
@@ -315,6 +315,36 @@ gitcheckoutremotebranch ()
     #fi;
 #}
 
+git-deletemultiplebranches ()
+{
+    local branch_filter=${1};
+    local branch_list=();
+    local branches=();
+    local delete_branches=();
+
+    if [ -n "${branch_filter}" ] ; then
+        branch_list=($(git b | grep ${branch_filter}| sed 's/\*//g'));
+    else
+        branch_list=($(git b | sed 's/\*//g'));
+    fi;
+
+    for b in ${branch_list[@]} ; do
+        branches+=($b "" off);
+    done;
+
+    # calulate size and hight of menu
+    #s=${#branches[@]};
+    #h=$((2 * $s));
+
+    delete_branches=($(whiptail --checklist "delete branch" 20 100 10 "${branches[@]}" 3>&1 1>&2 2>&3));
+    delete_branches=($(echo ${delete_branches[@]} | sed 's/"//g'));
+
+    for b in ${delete_branches[@]} ; do
+        echo -e  "${RED}git delete branch $b${NC}";
+        git bdelete $b;
+    done;
+}
+
 git-deletebranch ()
 {
     local branch=;
