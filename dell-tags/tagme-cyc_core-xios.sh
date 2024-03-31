ab91f71b3cde95342e4819321a4ad476a271529e
yonatan.cohen <yonatan.cohen@dell.com>
2024-03-31 08:12:08 +0300

    dell: added tagme script for xios and scsi driver
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

---
+++ dell-tags/tagme-cyc_core-xios.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source ${yonienv}/bashrc_tags.sh
+includeTagdir+=(cyc_platform/src/mbe_r/)
+includeTagdir+=(cyc_platform/src/module_r/)
+includeTagdir+=(cyc_platform/src/xios/)
+includeTagdir+=(cyc_platform/src/include/)
+
+
+main ()
+{
+    echo ${PRJ_NAME}
+    if [ $# -gt 0 ]  ; then 
+        rm -f cscope.*;    
+        rm -f tags;
+    fi
+
+    tagme_base includeTagdir[@] excludeTagdir[@]
+}
+
+main $@
