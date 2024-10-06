177335768786f38325110cf6c9a090625f21adc1
yonatan.cohen <yonatan.cohen@dell.com>
2024-10-06 17:44:22 +0300

    gitconfig: git smstatus.sh works faster
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git-smstatus.sh
+++ bin/git-smstatus.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-git sm foreach echo "$(git hh) $(git bb)"
+#git sm foreach echo "$(git hh) $(git bb)"
+for s in source/* ; do
+    cd $s ;
+        if [ $(ls |wc -l ) -eq 0 ] ; then
+            echo -e "\033[0;34m-                                         $s\033[0m" ;
+        else
+            echo -e "\033[0;32m+$(git hh) $s\033[0m" ;
+        fi;
+        cd - 2>&1 1>/dev/null ;
+    done;
+BLUE=""
