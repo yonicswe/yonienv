e687a1238dfc5e20df883fd34a1b28d47d754355
yonatan.cohen <yonatan.cohen@dell.com>
2024-10-22 10:01:59 +0300

    dell: add git_rebaseremotebranch.sh and revised recapture list
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git_rebaseremotebranch.sh
+++ bin/git_rebaseremotebranch.sh
@@ -3,6 +3,20 @@
 source ${yonienv}/bashrc_common.sh
 source ${yonienv}/bashrc_fs.sh
 
+cyclone_folder=$1;
+
+echo "cyclone_folder=${cyclone_folder}";
+exit;
+
+rebase_remote_branch_bkp_file=.rebase_remote_branch;
+
+branch=;
+
+#if [ -e ${rebase_remote_branch_bkp_file} ] ; then
+    #branch=${rebase_remote_branch_bkp_file};
+    #ask_user_default_yes "use ${branch} again ?"; 
+    #if [ ]
+
 ask_user_default_no "git fetch before we start ? ";
 if [ $? -eq 1 ] ; then
     git fetch origin;
@@ -27,3 +41,4 @@ echo "git fetch origin ${branch};";
 git fetch origin ${branch};
 echo "git rebase FETCH_HEAD;";
 git rebase FETCH_HEAD;
+echo ${branch} > ${rebase_remote_branch_bkp_file};
