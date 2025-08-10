49482ce70ac4f360d25c585c79235d45f4c30faf
yonatan.cohen <yonatan.cohen@dell.com>
2025-08-10 12:44:25 +0300

    git: rebaseremotebranch script can use a parameter for branch name
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git-rebaseremotebranch.sh
+++ bin/git-rebaseremotebranch.sh
@@ -3,32 +3,40 @@
 source ${yonienv}/bashrc_common.sh
 source ${yonienv}/bashrc_fs.sh
 
-cyclone_folder=$1;
-
-echo "cyclone_folder=${cyclone_folder}";
-exit;
-
 rebase_remote_branch_bkp_file=.rebase_remote_branch;
 
-branch=;
+branch=$1;
 
 #if [ -e ${rebase_remote_branch_bkp_file} ] ; then
     #branch=${rebase_remote_branch_bkp_file};
     #ask_user_default_yes "use ${branch} again ?"; 
     #if [ ]
 
-ask_user_default_no "git fetch before we start ? ";
-if [ $? -eq 1 ] ; then
-    git fetch origin;
-fi;
+if [ -z "${branch}" ] ; then
+    if [[ $(git br|wc -l) -eq 0 ]] ; then
+        echo "you probably just cloned linux repo you must also do git fetch" 
+        ask_user_default_no "continue with fetch ?";
+        if [[ $? -eq 0 ]] ; then
+            exit;
+        fi;
+        git fetch origin
+    else
+        ask_user_default_no "git fetch before we start ? ";
+        if [ $? -eq 1 ] ; then
+            git fetch origin;
+        fi;
+    fi;
 
-branch="$(git br | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+    branch="$(git br | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+    if [ -z "${branch}" ] ; then
+        exit;
+    fi;
+    branch=$(echo ${branch} | sed 's/origin\///g');
+fi;
 
 if [ -z "${branch}" ] ; then
     exit;
 fi;
-
-branch=$(echo ${branch} | sed 's/origin\///g');
 
 echo -e "\t${BLUE}git fetch origin ${GREEN}${branch}${NC}";
 echo -e "\t${BLUE}git rebase ${GREEN}FETCH_HEAD${NC}";
