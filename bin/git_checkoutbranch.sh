d9e5fe3441ce6539663a0dcaf04f2d401c806c25
yonatan.cohen <yonatan.cohen@dell.com>
2025-10-09 11:27:11 +0300

    git: git checkoutbranch can handle <branch> argument
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git_checkoutbranch.sh
+++ bin/git_checkoutbranch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-branch="$(git b | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+branch=${1}
+if [[ -z "${branch}" ]] ; then 
+    branch="$(git b | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+fi;
 
 if [ -n "${branch}" ] ; then
     echo "git checkout branch : ${branch}";
