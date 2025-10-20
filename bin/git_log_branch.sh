bf85062b69b889047de9574cbffa2bef8e969856
yonatan.cohen <yonatan.cohen@dell.com>
2025-10-20 10:21:01 +0300

    git: git_log_branch can also get a branch name instead of using fzf to offer user a choice
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git_log_branch.sh
+++ bin/git_log_branch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
-branch="$(git b | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+branch=${1};
+if [ -z ${branch} ] ; then
+    branch="$(git b | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
+fi;
 
 if [ -n "${branch}" ] ; then
     echo "git ll ${branch}";
