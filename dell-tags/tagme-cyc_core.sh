24d3be6f3b7a21da0c88b764b471a3719390824d
yonatan.cohen <yonatan.cohen@dell.com>
2026-01-26 16:05:44 +0200

    dell-tags: added sym to dellcyclonetagsupdate
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- dell-tags/tagme-cyc_core.sh
+++ dell-tags/tagme-cyc_core.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source ${yonienv}/bashrc_tags.sh
+includeTagdir+=(cyc_platform/src/sym/include/)
 includeTagdir+=(cyc_platform/src/sym/mbe/)
+includeTagdir+=(cyc_platform/src/sym/bl/)
 includeTagdir+=(cyc_platform/src/mbe_r/)
 includeTagdir+=(cyc_platform/src/module_r/)
