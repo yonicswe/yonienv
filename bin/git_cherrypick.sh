51c98b4692d732a222080456cb862bf8b0d80adf
yonatan.cohen <yonatan.cohen@dell.com>
2026-07-27 19:51:49 +0300

    git: cherry pick from branch imporved to take a range of commits
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- bin/git_cherrypick.sh
+++ bin/git_cherrypick.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if (( $# == 2 )) ; then
+    c1=$1;
+    c2=$2;
+    echo "git cherry-pick ${c2}^..${c1}"
+    git log --oneline --pretty=format:'%C(yellow) %h %Cred%<(9,trunc)%aN %Cgreen%s %Creset' ${c2}^..${c1}
+    read -p "continue [Y/n]" ans;
+    if [[ ${ans} =~ "n" ]] ; then
+        exit;
+    fi;
+    git cherry-pick ${c2}^..${c1}
+    exit;
+fi;
+
 branch="$(git b | fzf -0 -1 --border=rounded --height='20' | awk -F: '{print $1}')"
 
 if [ -n "${branch}" ] ; then
