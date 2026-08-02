b301447ee576dd7080cb4145d5a8d5afb23013e0
yonatan.cohen <yonatan.cohen@dell.com>
2026-08-02 12:52:59 +0300

    dell: fast_nvmet_driver_loader.sh script improved
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- scripts/fast_nvmet_driver_loader.sh
+++ scripts/fast_nvmet_driver_loader.sh
@@ -21,9 +21,14 @@ if [ -z ${cyclone_folder} ] ; then
     exit;
 fi;
 
-old_drivers_path=/cyc_software_0/cyc_host/cyc_common/modules/
 new_drivers_release_path=${cyclone_folder}/source/cyc_core/cyc_platform/obj_Release/package/final/top_host/cyc_host/cyc_common/modules;
 new_drivers_debug_path=${cyclone_folder}/source/cyc_core/cyc_platform/obj_Debug/package/final/top_host/cyc_host/cyc_common/modules;
+
+old_drivers_a=`./run_core_a.sh sudo find /cyc_software_0/ /cyc_software_1/ -name modules`
+old_drivers_b=`./run_core_b.sh sudo find /cyc_software_0/ /cyc_software_1/ -name modules`
+
+echo -e "${BLUE}found old drivers on node_a in ${old_drivers_a}${NC}";
+echo -e "${BLUE}found old drivers on node_b in ${old_drivers_b}${NC}";
 
 found_release=false;
 found_debug=false;
@@ -66,7 +71,7 @@ find ${new_driver_path} -type f -name "*ko" -print0 | tar --null -T - -cf new_dr
 if [[ $(./run_core_a.sh ls /home/core/  | grep modules.orig | wc -l  ) == 0 ]] ; then
     echo -e "${RED}backup original drivers to node-a://home/core/modules.orig${NC}";
     ./run_core_a.sh mkdir /home/core/modules.orig
-    ./run_core_a.sh cp ${old_drivers_path}/*.ko /home/core/modules.orig;
+    ./run_core_a.sh cp ${old_drivers_a}/*.ko /home/core/modules.orig;
 else
     echo -e "${BLUE}found backup of original drivers in node-a://home/core/modules.new ! skipping backup${NC}";
 fi;
@@ -75,33 +80,56 @@ fi;
 if [[ $(./run_core_b.sh ls /home/core/  | grep modules.orig | wc -l  ) == 0 ]] ; then
     echo -e "${RED}backup original drivers to node-b://home/core/modules.orig${NC}";
     ./run_core_b.sh mkdir /home/core/modules.orig
-    ./run_core_b.sh cp ${old_drivers_path}/*.ko /home/core/modules.orig;
+    ./run_core_b.sh cp ${old_drivers_b}/*.ko /home/core/modules.orig;
 else
     echo -e "${BLUE}found backup of original drivers in node-b://core/modules.new ! skipping backup${NC}";
 fi;
 
-# install drivers to node-a
-echo -e "${GREEN}./scp_core_to_a.sh new_drivers.tar${NC}";
-./scp_core_to_a.sh new_drivers.tar
+install_a=0;
+install_b=0;
+read -p "install a or b or Both [a/b/B]" ans;
+if [[ "${ans}" =~ 'a' || "${ans}" =~ 'B' ]] ; then
+    install_a=1;
+fi;
+if [[ "${ans}" =~ 'b' || "${ans}" =~ 'B' ]] ; then
+    install_b=1;
+fi;
+if [ -z "${ans}" ] ; then
+    install_a=1;
+    install_b=1;
+fi;
 
-echo -e "${GREEN}./run_core_a.sh tar xvf new_drivers.tar -C ${old_drivers_path}${NC}";
-./run_core_a.sh sudo tar xvf new_drivers.tar -C ${old_drivers_path};
+####### install step ############
+if [[ ${install_a} == 1 ]] ; then
+    # install drivers to node-a
+    echo -e "${GREEN}./scp_core_to_a.sh new_drivers.tar${NC}";
+    ./scp_core_to_a.sh new_drivers.tar
 
-# install drivers to node-b
-echo -e "${GREEN}./scp_core_to_b.sh new_drivers.tar${NC}";
-./scp_core_to_b.sh new_drivers.tar
+    echo -e "${GREEN}./run_core_a.sh sudo tar xvf new_drivers.tar -C ${old_drivers_a}${NC}";
+    ./run_core_a.sh sudo tar xvf new_drivers.tar -C ${old_drivers_a};
+fi;
 
-echo -e "${GREEN}./run_core_b.sh tar xvf new_drivers.tar -C ${old_drivers_path}${NC}";
-./run_core_b.sh sudo tar xvf new_drivers.tar -C ${old_drivers_path};
+if [[ ${install_b} == 1 ]] ; then
+    # install drivers to node-b
+    echo -e "${GREEN}./scp_core_to_b.sh new_drivers.tar${NC}";
+    ./scp_core_to_b.sh new_drivers.tar
 
-read -p  "about to restart both nodes [Enter]"
+    echo -e "${GREEN}./run_core_b.sh sudo tar xvf new_drivers.tar -C ${old_drivers_b}${NC}";
+    ./run_core_b.sh sudo tar xvf new_drivers.tar -C ${old_drivers_b};
+fi;
 
-echo -e "${GREEN}Reloading drivers on the node-a${NC}";
-./stack_down_hard_only_a.sh
-./stack_up_only_a.sh
+read -p  "about to restart nodes [Enter]"
 
-echo -e "${GREEN}Reloading drivers on the node-b${NC}";
-./stack_down_hard_only_b.sh
-./stack_up_only_b.sh
+if [[ ${install_a} == 1 ]] ; then
+    echo -e "${GREEN}Reloading drivers on the node-a${NC}";
+    ./stack_down_hard_only_a.sh
+    ./stack_up_only_a.sh
+fi;
+
+if [[ ${install_b} == 1 ]] ; then
+    echo -e "${GREEN}Reloading drivers on the node-b${NC}";
+    ./stack_down_hard_only_b.sh
+    ./stack_up_only_b.sh
+fi;
 
 exit 0;
