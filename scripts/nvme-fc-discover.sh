2bf5af1a551db5ab579686b9f773b8e7db0f895c
yonatan.cohen <yonatan.cohen@dell.com>
2026-08-11 10:09:07 +0300

    dell: nvme-fc-discover.sh saves results to file
    Signed-off-by: Yonatan Cohen <yonic.swe@gmail.com>

--- scripts/nvme-fc-discover.sh
+++ scripts/nvme-fc-discover.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# how to 
+# use bsclistnvmeportsfcc on the bsc to create the powerstore_file
+# then use bscscptohost to create a scp command to copy the powerstore_file;
+# use dellnvme-fc-host-nodename-portname on the host to create the host file
+# invoke ./nvme-fc-discover.sh host_file powerstore_file 
+# to test all possible paths between host and target
+
+RED="\033[1;31m"
+REDBLINK="\033[1;5;31m"
+REDITALIC="\033[1;3;31m"
+REDREVERSE="\033[1;7;31m"
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+PURPLE="\033[0;35m"
+BROWN="\033[0;33m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
+
 host_file=${1};
 powerstore_file=${2};
 
@@ -22,6 +41,9 @@ fi;
 host_arr=( $(cat $host_file) );
 powerstore_arr=( $(cat $powerstore_file) );
  
+success_file=successful_fc_connections.txt;
+echo > ${success_file};
+    
 for (( c=1, h=0; h<${#host_arr[@]} ; h++)) ; do
     for (( p=0; p<${#powerstore_arr[@]} ; p++)) ; do
         cmd="nvme discover -t fc -a ${powerstore_arr[${p}]} -w ${host_arr[${h}]}";
@@ -31,9 +53,14 @@ for (( c=1, h=0; h<${#host_arr[@]} ; h++)) ; do
             exit 1;
         fi;
         eval $cmd;
+        if [[ $? -eq 0 ]] ; then
+            echo -e "$c\n${cmd}" >> ${success_file};
+        fi;
         ((c++));
     done;
 done;
+
+echo -e "${RED}results written to ${success_file}${NC}";
 
 
 
